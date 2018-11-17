9352731b63e268854251f7d2ffc84f0ab9bd4b69
pixelvega <pixelvega.info@gmail.com>
2018-11-17 16:29:52 +0100

    Mi aplicación Django Girls, primer commit

---
+++ python_intro.py
@@ -0,0 +1,35 @@
+# print('Hello, Django girls!')
+# if 5 > 2:
+#     print('5 is indeed greater than 2')
+# else:
+#     print('5 is not greater than 2')
+# name = "Lucia"
+# if name == "Paula":
+#     print ('hey hola ' + name)
+# elif name == 'Lucia':
+#     print('hey hola!') 
+# else:
+#     print('hey anonymous')
+
+# def hi(name):
+#     print('Hi there!' + name)
+#     print('How are you?')
+#hi("paula")
+# name= 'Carol'
+# def goodbye(name):
+#     if name == "Carol":
+#         print('Goodbye' + name)
+#     elif name == "Emma":
+#         print('Goodbye' + name)
+#     else:
+#         print('Goodbye nobody')
+# goodbye('Emma')
+
+def hi(name):
+    print('Hi ' + name + '!')
+adalabers = ['Carol', 'Emma', 'Yen', 'Paula', 'Paula2']
+for name in adalabers:
+    hi(name)
+    print('Next girl: ')
+for i in range(1, 6):
+    print(i)
